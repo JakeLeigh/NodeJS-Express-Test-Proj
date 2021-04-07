c123024525a9d9460d68a7d6381749e11a8f449c
Jake Leigh <jake@jakeleigh.io>
2021-04-07 09:32:33 +0100

    configure express to use pug as view templates

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || '8000';
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
   res.status(200).send('Node.JS App');
 });
