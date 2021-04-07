f4fe7fdd4c33968ed3b3e27190d27a4ee396848d
Jake Leigh <jake@jakeleigh.io>
2021-04-07 09:23:30 +0100

    define home route and listen command to log successful call

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || '8000';
+
+app.get('/', (req, res) => {
+  res.status(200).send('Node.JS App');
+});
+
+app.listen(port, () => {
+  console.log(`Listening to requests on port ${port}`);
+})
